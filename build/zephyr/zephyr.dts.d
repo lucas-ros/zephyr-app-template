0bde12acb62b2d4a9d1eb1e889edc23b89801225
lucas-ros <auslc15@gmail.com>
2026-02-05 10:58:03 -0300

    create basic structure

---
+++ build/zephyr/zephyr.dts.d
@@ -0,0 +1,40 @@
+empty_file.o: /work/zephyr/misc/empty_file.c \
+ /work/zephyr/boards/st/nucleo_f410rb/nucleo_f410rb.dts \
+ /work/zephyr/dts/arm/st/f4/stm32f410Xb.dtsi \
+ /work/zephyr/dts/arm/st/f4/stm32f410.dtsi \
+ /work/zephyr/include/zephyr/dt-bindings/clock/stm32f410_clock.h \
+ /work/zephyr/include/zephyr/dt-bindings/clock/stm32f4_clock.h \
+ /work/zephyr/include/zephyr/dt-bindings/clock/stm32_common_clocks.h \
+ /work/zephyr/dts/arm/st/f4/stm32f4.dtsi \
+ /work/zephyr/dts/arm/armv7-m.dtsi /work/zephyr/dts/common/skeleton.dtsi \
+ /work/zephyr/include/zephyr/dt-bindings/adc/adc.h \
+ /work/zephyr/include/zephyr/dt-bindings/dt-util.h \
+ /work/zephyr/include/zephyr/sys/util_macro.h \
+ /work/zephyr/include/zephyr/sys/util_internal.h \
+ /work/zephyr/include/zephyr/sys/util_loops.h \
+ /work/zephyr/include/zephyr/sys/util_listify.h \
+ /work/zephyr/include/zephyr/sys/util_internal_is_eq.h \
+ /work/zephyr/include/zephyr/sys/util_internal_util_inc.h \
+ /work/zephyr/include/zephyr/sys/util_internal_util_dec.h \
+ /work/zephyr/include/zephyr/sys/util_internal_util_x2.h \
+ /work/zephyr/include/zephyr/dt-bindings/clock/stm32f4_clock.h \
+ /work/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ /work/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ /work/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ /work/zephyr/include/zephyr/dt-bindings/pwm/stm32_pwm.h \
+ /work/zephyr/include/zephyr/dt-bindings/dma/stm32_dma.h \
+ /work/zephyr/include/zephyr/dt-bindings/adc/stm32f4_adc.h \
+ /work/zephyr/include/zephyr/dt-bindings/adc/stm32_adc.h \
+ /work/zephyr/include/zephyr/dt-bindings/reset/stm32f2_4_7_reset.h \
+ /work/zephyr/include/zephyr/dt-bindings/reset/stm32-common.h \
+ /work/zephyr/include/zephyr/dt-bindings/sensor/qdec_stm32.h \
+ /work/zephyr/include/zephyr/dt-bindings/power/stm32_pwr.h \
+ /work/zephyr/dts/common/freq.h /work/zephyr/dts/common/mem.h \
+ /work/modules/hal/stm32/dts/st/f4/stm32f410r(8-b)tx-pinctrl.dtsi \
+ /work/zephyr/include/zephyr/dt-bindings/pinctrl/stm32-pinctrl.h \
+ /work/zephyr/include/zephyr/dt-bindings/pinctrl/stm32-pinctrl-common.h \
+ /work/zephyr/boards/st/nucleo_f410rb/arduino_r3_connector.dtsi \
+ /work/zephyr/include/zephyr/dt-bindings/gpio/arduino-header-r3.h \
+ /work/zephyr/boards/st/nucleo_f410rb/st_morpho_connector.dtsi \
+ /work/zephyr/include/zephyr/dt-bindings/gpio/st-morpho-header.h \
+ /work/zephyr/include/zephyr/dt-bindings/input/input-event-codes.h
